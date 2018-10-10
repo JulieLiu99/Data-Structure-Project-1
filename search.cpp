2b94fb25454ffc217a29d8c1a01be4df73a0fc00
Romeno Wenogk Fernando <romenowenogk@gmail.com>
2018-10-10 23:44:45 +0400

    Commented and cleaned code (pls see if it still works xD)

--- search.cpp
+++ search.cpp
@@ -69,7 +69,6 @@ void dSearch::binaryWordStarSearch(vector<string> A, int n, string value, int wo
     } else {
     	cout << "word found" << endl;
         cout << comparison << " word comparisons carried out" << endl;
-        //cout << "end: " << endIndex << "... start: " << startIndex << endl;
         if((endIndex-startIndex)>0) { //If there is more than one matched word in the search ( endIndex!=startIndex would work too )
         for(int j=0;j<(endIndex+1-startIndex);j++) { //Run loop from startIndex to endIndex
         	if(j<wordcount) { //As long as j is less than the wordcount specified by the user
@@ -84,70 +83,60 @@ void dSearch::binaryWordStarSearch(vector<string> A, int n, string value, int wo
 
 void dSearch::binaryWordQSearch(vector<string> A, int n, int qMarkPos, string value, int wordcount) {
 
-	string frontStringPart = value.substr(0,qMarkPos);
-	string backStringPart = value.substr(qMarkPos+1,value.size()-1);
-	int first = 0;
-    int last = n-1;
+	string frontStringPart = value.substr(0,qMarkPos); //The substring of the search query from the first character to the ? mark character excluding the question mark
+	string backStringPart = value.substr(qMarkPos+1,value.size()-1); //The substring of the search query from the ? mark character to the last character
+	int first = 0; // The first element in the vector A
+    int last = n-1; // The last element in the vector A
     int middle;
     int position;
-    bool found = false;
-    int comparison = 0;
+    bool found = false; //Boolean for whether a match for the frontStringPart to be found in the start of any word in the dictionary
+    int comparison = 0; //The number of word comparisons carried out for the current search
     
     while (!found && first <= last) {
-        middle = (first + last)/2;
-        if (A[middle].find(frontStringPart) == 0){ 
+        middle = (first + last)/2; //The index halfway between the first and last variables
+        if (A[middle].find(frontStringPart) == 0){ //If the middle value itself starts with the frontStringPart
             found = true;
             position = middle;
-        } else if (A[middle] > frontStringPart) last = middle - 1;
-        else first = middle + 1;
-        comparison += 1;
+        } else if (A[middle] > frontStringPart) last = middle - 1; // If the middle value is higher than the search value alphabetically, make the last index the middle-1 index
+        else first = middle + 1; // If the middle value is lower than the search value alphabetically, make the first index the middle + 1 index
+        comparison += 1; // Increase the comparison by 1 every time the loop is run to update the number of word comparisons
     }
 
-    int currentWord=position;
-    int startIndex=position+1;
-    while(A[startIndex-1].find(frontStringPart) == 0) {
-    	startIndex=startIndex-1;
+    int startIndex=position+1; //Index to start the search for matching values to the left of the current position index
+    while(A[startIndex-1].find(frontStringPart) == 0) { //While the previous value to the current startIndex STARTS with frontStringPart 
+    	startIndex=startIndex-1; //Reduce the startIndex by one 
     }
-    int endIndex=position;
-        while(A[endIndex+1].find(frontStringPart) == 0) {
-    	endIndex=endIndex+1;
+    int endIndex=position; //Index to start the search for matching values to the right of the current position index
+        while(A[endIndex+1].find(frontStringPart) == 0) { //While the next value to the current endIndex STARTS with frontStringPart
+    	endIndex=endIndex+1; //Increase the endIndex by one 
     }
- 	int first2= startIndex;
- 	int last2= endIndex;
+ 	int first2= startIndex; //The starting index for the range of words that start with the substring before the ? mark denoted by the string frontStringPart
+ 	int last2= endIndex; //The last index for the range of words that start with the substring before the ? mark denoted by the string frontStringPart
  	
- 	bool foundFinal = false;
- 	int finalPosition;
- 	string curBackStringPart;
- 	int wordCounter=0;
+ 	bool foundFinal = false; //Boolean for whether word(s) are matched exactly to the search query 
+ 	int finalPosition; 
+ 	string curBackStringPart; 
+ 	int wordCounter=0; //The number of words matched which will be limit what is printed out by the wordcount preferred by the user
  	
  	for(int j=0;j<(last2+1-first2);j++) {
- 		comparison+=1;
- 		if(A[first2+j].size()==value.size()) {
- 		//cout << "CURRENT WORD IN LOOP---> " << A[first2+j] << endl;
- 		curBackStringPart = A[first2+j].substr(qMarkPos+1,A[first2+j].size()-1);
- 		//cout << "curBackStringPart: " << curBackStringPart << endl;
- 		if((curBackStringPart==backStringPart)) {
+ 		comparison+=1; // Increase the comparison by 1 every time the loop is run to update the number of word comparisons
+ 		if(A[first2+j].size()==value.size()) { //If the current word in loop and the search query have the same legnth
+ 		curBackStringPart = A[first2+j].substr(qMarkPos+1,A[first2+j].size()-1); //The substring formed from the current word in the loop from the index of the ? mark in the query string to to last character
+ 		if((curBackStringPart==backStringPart)) { //If the 'virtual' substring formed above matches the backStringPart, then we know that this is a match as the substring before and after the ? mark match  
  			
- 			foundFinal=true;
+ 			foundFinal=true; 
  			finalPosition=first2+j;
- 			//cout << "wordCounter: " << wordCounter << " - wordcount: " << wordcount << endl;
- 			if((wordCounter<wordcount)) {
- 			cout << A[first2+j] << endl;
+ 			if((wordCounter<wordcount)) { //Limit words printed out to user specified number, wordcount
+ 			cout << A[first2+j] << endl; //Print matched word
  		}
- 		wordCounter+=1;
+ 		wordCounter+=1; //MIGHT NEED TO GO IN THE if((curBackStringPart==backStringPart))
  		}
  	}
  }
- 	//cout << "first2: " <<first2 << " last2: " << last2 << "backStringPart: " << backStringPart << "curBackStringPart: " << curBackStringPart << " Looking in: " << A[position] << endl;
     if(!foundFinal) {
     	cout<< "word not found" << endl;
     } else {
     	cout << "word found" << endl;
         cout << comparison << " word comparisons carried out" << endl;
-        //cout << "end: " << endIndex << "... start: " << startIndex << endl;
-     	/*for(int j=0;j<(last2+1-first2);j++) {
-    	cout << A[first2+j] << endl;
-    }*/
-    
     }
 }
